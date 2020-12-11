153426deac145e4ca687ecf4aa88bc4800286334
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2020-12-11 17:34:17 +0100

    init ec2

--- support/installEC2.sh
+++ support/installEC2.sh
@@ -34,5 +34,6 @@ cp support/swift-t-files/tcl-julia.c swift-t/turbine/code/src/tcl/julia/tcl-juli
 
 swift-t/dev/build/build-swift-t.sh
 
-export PATH=$PATH:/home/ubuntu/EQ-JL/swift-t-install/stc/bin:/home/ubuntu/EQ-JL/swift-t-install/turbine/bin
-
+echo 'export PATH=$PATH:/home/ubuntu/EQ-JL/swift-t-install/stc/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:/home/ubuntu/EQ-JL/swift-t-install/turbine/bin' >> ~/.bashrc
+source ~/.bashrc
